d415bbe76b51bb2c27e6a0d0c1853fac73629242
monellz <monell.zhong@gmail.com>
2021-07-16 14:09:45 +0800

    run script on gorgon

--- run_gorgon.sh
+++ run_gorgon.sh
@@ -1,7 +1,7 @@
 PROCS=8
-THREADS=8
+THREADS=2
 
 export OMP_NUM_THREADS=${THREADS}
-export OMP_DISPLAY_ENV=true
+# export OMP_DISPLAY_ENV=true
 
-srun -n ${PROCS} -N 4 -p CPU bin/./graphClustering -i -f data/60w_vertices.bin
+srun -n ${PROCS} -N 4 -p CPU bin/./graphClustering -i -f data/60w_vertices.bin 2>/dev/null
